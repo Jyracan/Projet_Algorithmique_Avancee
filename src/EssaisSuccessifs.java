ad577e6ec90dc7634f15105eed7e15617e9bb3d0
jyra <cmoineau@enssat.fr>
2019-04-02 18:16:50 +0200

    Début de code ...

--- src/EssaisSuccessifs.java
+++ src/EssaisSuccessifs.java
@@ -1,2 +1,13 @@
+import utils.Parser;
+import utils.Point;
+
+import java.util.Set;
+
 public class EssaisSuccessifs {
+
+    public static void main(String[] args) {
+        Set<Point> points = Parser.recuperePoints();
+        boolean[] X = new boolean[points.size()-2];
+
+    }
 }

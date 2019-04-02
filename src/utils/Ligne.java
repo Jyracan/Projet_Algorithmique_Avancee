cc2dd055c53e4aae46450e13d9ffd37f30f5d7c3
jyra <cmoineau@enssat.fr>
2019-04-02 18:03:34 +0200

    Ajout des fichiers utilitaire

---
+++ src/utils/Ligne.java
@@ -0,0 +1,17 @@
+package utils;
+
+import java.awt.*;
+
+public class Ligne {
+	private Point point1;
+	private Point point2;
+
+	public Ligne(Point p1, Point p2 ){
+		point1=p1;
+		point2=p2;
+	}
+	
+	Point getp1(){return point1;}
+	Point getp2(){return point2;}
+
+}

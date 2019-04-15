0fc38f8323f919ef8ae2782d42583e7272f9f4aa
Rémy Cordeau-Mirani <rcordeau@enssat.fr>
2019-04-15 13:05:59 +0200

    début de procédure appligbri recursive

--- src/EssaisSuccessifs.java
+++ src/EssaisSuccessifs.java
@@ -13,7 +13,7 @@ public class EssaisSuccessifs {
 
     public static void appligbri (HashSet<Point> setPoint){
         int nbPoints = setPoint.size();
-        boolean[] X = new boolean[nbPoints-2]; // On crée notre tableau de point, on ne met pas le premier ni le dernier point dans ce tableau
+        boolean[] X = new boolean[nbPoints-2]; // On crée notre tableau de points, on ne met pas le premier ni le dernier point dans ce tableau
         for (boolean point: X) point = false;   //On dit que tous les points ne sont pas dans le segment de droite
 
         Point [] points = transformToTab(setPoint);
@@ -26,7 +26,7 @@ public class EssaisSuccessifs {
             for (int i = cpt; i<nbPoints-2; i++){
                 X[i]= true;
                 scoreTMP = calculCout(X,points);
-                if(scoreTMP < scoreOpt) {scoreOpt = scoreTMP;  Xopt=X;}
+                if(scoreTMP < scoreOpt) {scoreOpt = scoreTMP;  Xopt=X.clone();}
             }
             for (boolean point: X) point = false;
             cpt ++;
@@ -35,9 +35,40 @@ public class EssaisSuccessifs {
 
     }
 
+    public static void appligbri_rec(HashSet<Point> setPoint, boolean[] X, Point [] points ,int i){
+
+        int xi;
+        double score;
+
+        for(xi = 0; xi < 2; xi++){
+            if(X[i] == false) { // satisfaisant(xi) : le point n'a pas été pris
+                X[i] = intToBoolean(xi); //enregistrer(xi)
+                score = calculCout(X,points);
+                System.out.println("score : " + score);
+                if(i == X.length - 1){
+                    visualizeRes(points,X,score);
+                }else{
+                    appligbri_rec(setPoint,X,points,i+1);
+                }
+                X[i] = !intToBoolean(xi);
+            }
+        }
+
+    }
+
+    private static boolean intToBoolean(int i){
+        if(i == 0){
+            return false;
+        }
+        else if(i == 1){
+            return true;
+        }
+        return Boolean.parseBoolean(null);
+    }
+
     private static Point[] transformToTab (HashSet<Point> setPoint){
         int nbPoints = setPoint.size();
-        //Initialisation du tableau de point pour les obtenirs plus facilement
+        //Initialisation du tableau de points pour les obtenir plus facilement
         Point[] points = new Point[nbPoints];
         Iterator<Point> it = setPoint.iterator();
         Point tmp;
@@ -114,9 +145,13 @@ public class EssaisSuccessifs {
 
     public static void main(String[] args) {
         HashSet<Point> setPoint = (HashSet<Point>) Parser.recuperePoints();    //On récupère un set de point
+        //appligbri(setPoint);
+        boolean[] X = new boolean[setPoint.size() - 2];
+        for(boolean point: X){
+            point = false;
+        }
 
-        appligbri(setPoint);
-
-
+        Point [] points = transformToTab(setPoint);
+        appligbri_rec(setPoint,X,points,0);
     }
 }

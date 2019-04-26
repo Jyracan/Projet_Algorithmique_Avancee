788de3aa9dd3c3513d3befa2b279942c31c10344
rcordeau <remy.cordeau@yahoo.fr>
2019-04-26 11:57:16 +0200

    maj essais successifs opt

--- src/EssaisSuccessifs.java
+++ src/EssaisSuccessifs.java
@@ -7,37 +7,47 @@ public class EssaisSuccessifs {
 
     private static HashMap<Double,boolean[]> solutionsPossibles;
     private static int cpt = 0;
-    private static Ligne dernierSegment;
     private static double scoreOpt;
 
     public static void appligibri_opt(Point[] points,boolean[] X,int i) {
-        if(i <= X.length){
-            if(satisfaisant(i,X) && elagage(points[i])) {
-                X[i-1]=true;
-                Point point = dernierSegment.getp1();
-                dernierSegment = new Ligne(dernierSegment.getp2(),points[i]);
-                if(optimal(points,X)){
-                    scoreOpt = UtilsSolver.calculCout(X,points);
-                    if(i == X.length){
-                        cpt++;
-                        solutionsPossibles.put(scoreOpt,X.clone());
-                    }else{
-                        appligibri_opt(points,X,i+1);
-                    }
-                }
-               X[i-1] = false;
-                dernierSegment = new Ligne(point,dernierSegment.getp1());
-                appligibri_opt(points,X,i+1);
-            }else {
-                X[i-1] = false;
-                if(i == X.length && optimal(points,X)){
-                    cpt++;
-                    solutionsPossibles.put(scoreOpt,X.clone());
-                }
-                appligibri_opt(points,X,i+1);
-            }
+       int[] Si = new int[]{0,1};
+       for(int xi: Si){
+           if (satisfaisant(i, X)){
+               enregistrer(i,xi,X);
+               cpt++;
+               if (soltrouvee(i,X)){
+                   solutionsPossibles.put(scoreOpt,X.clone());
+               }else {
+                   if(elagage(points,X)){
+                       scoreOpt = UtilsSolver.calculCout(X,points);
+                       appligibri_opt(points,X,i+1);
+                   }
+               }
+               defaire(i,X);
+           }
+       }
+    }
+
+    public static void enregistrer(int i,int xi,boolean[] X){
+        if(xi == 1){
+            X[i-1] = true;
+        }
+        else{
+            X[i-1] = false;
         }
     }
+
+    public static void defaire(int i, boolean[] X){
+        X[i-1] = false;
+    }
+
+    public static boolean soltrouvee(int i,boolean[] X){
+        if(i == X.length){
+            return true;
+        }
+        return false;
+    }
+
 
     public static boolean satisfaisant(int i,boolean[] X){
         if(i <= X.length){
@@ -46,19 +56,13 @@ public class EssaisSuccessifs {
         return false;
     }
 
-    public static boolean optimal(Point[] points, boolean[] X){
+    public static boolean elagage(Point[] points, boolean[] X){
         if(UtilsSolver.calculCout(X,points) <= scoreOpt){
             return true;
         }
         return false;
     }
 
-    public static boolean elagage(Point point ){
-        if(dernierSegment.distance(point) > 2* UtilsSolver.PENALITE){
-            return true;
-        }
-        return false;
-    }
 
     public static void main(String[] args) {
         HashSet<Point> setPoint = (HashSet<Point>) Parser.recuperePoints();    //On récupère un set de point
@@ -69,7 +73,6 @@ public class EssaisSuccessifs {
         boolean[] X =  new boolean[setPoint.size() - 2];
         for(boolean b: X) b = false;
 
-        dernierSegment = new Ligne(points[0],points[setPoint.size()-1]);
         solutionsPossibles = new HashMap<>();
         scoreOpt = UtilsSolver.calculCout(X,points);
 

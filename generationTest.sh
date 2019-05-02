a63fd91a63d1cf83e4576566f8381b1f97d04321
jyra <cmoineau@enssat.fr>
2019-05-02 15:20:08 +0200

    Ajout de fichier test

--- generationTest.sh
+++ generationTest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-for j in `seq 1 20`	
-do
-	for i in `seq 1 $j`	
+#for j in `seq 1 20`	
+#do
+	for i in `seq 1 2350`	
 	do
-		echo $i $((RANDOM%$j)) >> ./test/$j.txt
+		echo $i $((RANDOM%2350)) >> ./test/2350.txt
 	done
-done
+#done
 
 exit 0
 
